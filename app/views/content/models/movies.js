7342281b5af0d6f9e345c5429b4bbf6bddf11381
Mohammad <msalam6@my.centennialcollege.com>
2022-11-09 18:08:20 -0500

    Created Schema, will need revising later

--- app/views/content/models/movies.js
+++ app/views/content/models/movies.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
-//needs to be worked on
+//needs to be worked on, what else should be added to the schema
 const SurveySchema = new Schema({
     Question: String, 
     inputOption: Number, 
     Title: String,
     
 }, {
-        collection: 'movies'
+    
+    collection: 'surveys'
 });
 
-export default mongoose.model('Movies', MovieSchema);
+export default mongoose.model('Surveys', SurveySchema);

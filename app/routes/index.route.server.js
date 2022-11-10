6d4d5253ba36e751e2b9b36c32134c1199b64ed1
Mohammad <msalam6@my.centennialcollege.com>
2022-11-09 20:19:10 -0500

    edited controllers and routes

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -2,16 +2,14 @@ import { Router } from "express";
 import { displayAboutPage, 
     displayContactPage, 
     displayHomePage, 
-    displayProjectsPage, 
-    displayServicesPage } from "../controllers/index.controller.server.js";
+    } from "../controllers/index.controller.server.js";
 
 const router = Router();
 
 router.get('/', displayHomePage);
 router.get('/home', displayHomePage);
 router.get('/about', displayAboutPage);
-router.get('/projects', displayProjectsPage);
-router.get('/services', displayServicesPage);
 router.get('/contact', displayContactPage);
+
 
 export default router;
--- app/routes/movies.route.server.js
+++ app/routes/movies.route.server.js
@@ -1,21 +1,22 @@
 import { Router } from "express";
 
-import {  DisplayMoviesList, 
-    DisplayMoviesAddPage, 
-    ProcessMoviesAddPage, 
-    ProcessMoviesEditPage, 
-    DisplayMoviesEditPage, 
-    ProcessMoviesDelete } from "../controllers/movies.controller.server.js";
+import { 
+    DisplaySurveysAddPage,
+    DisplaySurveyList,
+    ProcessSurveysAddPage,
+    ProcessSurveysEditPage,
+    DisplaySurveysEditPage,
+    ProcessSurveyDeletePage} from "../controllers/surveys.controller.server.js";
 
     import { AuthGaurd } from "../utils/index.js";
 
 const router = Router();
 
-router.get('/movie-list', DisplayMoviesList);
-router.get('/movie-add', AuthGaurd,DisplayMoviesAddPage);
-router.post('/movie-add', AuthGaurd,ProcessMoviesAddPage);
-router.post('/movie-edit/:id',AuthGaurd, ProcessMoviesEditPage);
-router.get('/movie-edit/:id',AuthGaurd, DisplayMoviesEditPage);
-router.get('/movie-delete/:id',AuthGaurd, ProcessMoviesDelete);
+router.get('/survey-list', DisplaySurveyList);
+router.get('/survey-add', AuthGaurd,DisplaySurveysAddPage);
+router.post('/survey-add', AuthGaurd,ProcessSurveysAddPage);
+router.post('/survey-edit/:id',AuthGaurd, ProcessSurveysEditPage);
+router.get('/survey-edit/:id',AuthGaurd, DisplaySurveysEditPage);
+router.get('/movie-delete/:id',AuthGaurd, ProcessSurveyDeletePage);
 
 export default router;

d5918ed9c24f29a4df1233b122b53c5a19726bf4
Mohammad <msalam6@my.centennialcollege.com>
2022-11-09 20:19:22 -0500

    editied controllers and routes

---
+++ app/controllers/surveys.controller.server.js
@@ -0,0 +1,89 @@
+import surveyModel from '../views/content/models/movies.js';
+
+
+
+import { UserDisplayName } from '../utils/index.js';
+
+export function DisplaySurveyList(req, res, next){
+    surveyModel.find(function(err, moviesCollection) {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', {title: 'Survey List', page: 'surveys/list', movies: moviesCollection, displayName: UserDisplayName(req)});
+    })
+}
+
+export function DisplaySurveysAddPage(req, res, next){
+    res.render('index', { title: 'Add a survey', page: 'surveys/edit', movie: {}, displayName: UserDisplayName(req) });
+}
+
+export function ProcessSurveysAddPage(req, res, next){
+    
+    let newSurvey = surveyModel({
+        name: req.body.name,
+        year: req.body.year,
+        director: req.body.director,
+        genre: req.body.genre,
+        runtime: req.body.runtime
+    });
+
+    surveyModel.create(newMovie, (err, Movie) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/survey-list')
+    } )
+}
+
+export function DisplaySurveysEditPage(req, res, next){
+    let id = req.params.id;
+
+    surveyModel.findById(id, (err, movie) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', { title: 'Edit Survey', page: 'surveys/edit', movie: movie, displayName: UserDisplayName(req) });
+    });    
+}
+
+export function ProcessSurveysEditPage(req, res, next){
+
+    let id = req.params.id;
+    
+    let newSurvey = surveyModel({
+        _id: req.body.id,
+        name: req.body.name,
+        year: req.body.year,
+        director: req.body.director,
+        genre: req.body.genre,
+        runtime: req.body.runtime
+    });
+
+    surveyModel.updateOne({_id: id }, newMovie, (err, Movie) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/survey-list')
+    } )
+}
+
+export function ProcessSurveyDeletePage(req, res, next){
+    let id = req.params.id;
+
+    surveyModel.remove({_id: id}, (err) => {
+        if (err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/survey-list');
+    })
+}
